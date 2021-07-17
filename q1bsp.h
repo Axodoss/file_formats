47d02a2f8159252d16fc542eef8992fafc1490d7
Lukas Heise <lukas.heise@gmail.com>
2021-07-17 22:48:36 +0200

    Create q1bsp.h

---
+++ q1bsp.h
@@ -0,0 +1,21 @@
+
+#ifndef __Q1BSP_H
+#define __Q1BSP_H
+
+// https://www.gamers.org/dEngine/quake/spec/quake-spec34/qkspec_4.htm
+
+namespace q1bsp
+{
+
+}
+
+#endif
+
+#ifdef Q1BSP_IMPLEMENTATION
+
+namespace q1bsp
+{
+
+}
+
+#endif
