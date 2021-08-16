96bf0ed51998c31a5fe08d36350624c9e6553325
Lukas Heise <lukas.heise@gmail.com>
2021-08-16 15:32:43 +0200

    Update wad.h

--- code/wad.h
+++ code/wad.h
@@ -2,9 +2,25 @@
 #ifndef __WAD_H
 #define __WAD_H
 
+// https://zdoom.org/wiki/WAD
+
 namespace wad
 {
+	struct header_t
+	{
+		char id[4]; // IWAD or PWAD
+		int number_of_entries;
+		int offset;
+	};
 
+	struct lump_t
+	{
+		int offset;
+		int length;
+		char name[8] = {0};
+	};
+
+	bool LoadFromMemory(const void *file_data, int file_size, file_content_t *result);
 }
 
 #endif
@@ -13,7 +29,11 @@ namespace wad
 
 namespace wad
 {
-
+	bool LoadFromMemory(const void *file_data, int file_size, file_content_t *result)
+	{
+		// TODO
+		return false;
+	}
 }
 
 #endif
